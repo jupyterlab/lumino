e921e98d0a547aad78991ffc8782aa93cba900cc
Mehmet Bektas <mbektas@bloomberg.net>
2019-10-11 15:33:28 -0400

    viewport occluder, input validator, refactoring

--- packages/datagrid/src/basicmousehandler.ts
+++ packages/datagrid/src/basicmousehandler.ts
@@ -30,7 +30,7 @@ import {
 import {
   SelectionModel
 } from './selectionmodel';
-import { ICellEditResponse } from './celleditor';
+import { ICellEditResponse, ICellInputValidatorResponse } from './celleditor';
 
 /**
  * A basic implementation of a data grid mouse handler.
@@ -523,11 +523,23 @@ class BasicMouseHandler implements DataGrid.IMouseHandler {
         grid: grid,
         row: row,
         column: column,
-        metadata: grid.dataModel!.metadata('body', row, column)})
+        metadata: grid.dataModel!.metadata('body', row, column)},
+        {
+          validate: (value: any): ICellInputValidatorResponse => {
+            return {
+              valid: true
+            };
+          }
+        })
       .then((response: ICellEditResponse) => {
         if (grid.dataModel instanceof MutableDataModel) {
           const dataModel = grid.dataModel as MutableDataModel;
           dataModel.setData('body', row, column, response.value);
+        }
+        grid.viewport.node.focus();
+        if (response.returnPressed) {
+          grid.selectionModel!.incrementCursor();
+          grid.scrollToCursor();
         }
       });
     }
