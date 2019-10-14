eca800ed2df28d87843e55d0e167391f03074800
telamonian <mklein@jhu.edu>
2019-10-14 14:00:08 -0400

    taking a stab at allowing recursive cleanup for hpass vdom elements

--- packages/widgets/src/tabbar.ts
+++ packages/widgets/src/tabbar.ts
@@ -1343,8 +1343,8 @@ namespace TabBar {
       const { title } = data;
       let className = this.createIconClass(data);
 
-      if (title.iconRender) {
-        return hpass(title.iconRender, 'div');
+      if (title.iconRenderer) {
+        return hpass(title.iconRenderer, 'div');
       } else {
         return h.div({className}, data.title.iconLabel);
       }
