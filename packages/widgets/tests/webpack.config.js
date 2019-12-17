4012f86a75b02f2fe6c2fcba600b82e66b6b7c5d
Steven Silvester <stslve@amazon.com>
2019-12-17 12:34:41 -0600

    update webpack

--- packages/widgets/tests/webpack.config.js
+++ packages/widgets/tests/webpack.config.js
@@ -2,15 +2,8 @@ var path = require('path');
 
 module.exports = {
   entry: './build/index.spec.js',
+  mode: 'development',
   output: {
     filename: './build/bundle.test.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
   }
 }
