e5c53d3b8c8e5a703835cbec84eab72cdbfa3ac7
krassowski <5832902+krassowski@users.noreply.github.com>
2021-12-22 22:48:46 +0000

    Do not update textContent if it has not changed

--- packages/virtualdom/src/index.ts
+++ packages/virtualdom/src/index.ts
@@ -1349,7 +1349,10 @@ namespace Private {
 
       // Handle the simplest case of in-place text update first.
       if (oldVNode.type === 'text' && newVNode.type === 'text') {
-        currElem!.textContent = newVNode.content;
+        // Only update the textContent if it changed
+        if (currElem!.textContent !== newVNode.content) {
+          currElem!.textContent = newVNode.content;
+        }
         currElem = currElem!.nextSibling;
         continue;
       }
