04fa234dbf8d454753b56cda981509ee6b3e9b0e
Afshin T. Darian <git@darian.email>
2022-07-21 01:34:06 +0100

    Fix trailing-edge throttling with arguments.

--- packages/polling/src/ratelimiter.ts
+++ packages/polling/src/ratelimiter.ts
@@ -164,18 +164,20 @@ export class Throttler<
     options?: Throttler.IOptions | number
   ) {
     super(fn, typeof options === 'number' ? options : options && options.limit);
-    let edge: 'leading' | 'trailing' = 'leading';
     if (typeof options !== 'number') {
       options = options || {};
-      edge = 'edge' in options ? options.edge! : edge;
+      this._edge = 'edge' in options ? options.edge! : this._edge;
     }
-    this._interval = edge === 'trailing' ? this.limit : Poll.IMMEDIATE;
+    this._interval = this._edge === 'trailing' ? this.limit : Poll.IMMEDIATE;
   }
 
   /**
    * Throttles function invocations if one is currently in flight.
    */
   invoke(...args: V): Promise<T> {
+    if (this._edge === 'trailing') {
+      this.args = args;
+    }
     if (this.poll.state.phase !== 'invoked') {
       this.args = args;
       void this.poll.schedule({ interval: this._interval, phase: 'invoked' });
@@ -183,6 +185,7 @@ export class Throttler<
     return this.payload!.promise;
   }
 
+  private _edge: 'leading' | 'trailing' = 'leading';
   private _interval: number;
 }
 
