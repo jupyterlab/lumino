88520a2c3db44ae7cada33a06595dea44ab47c71
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-09-02 15:31:06 +0200

    finish adding rollup to examples

--- rollup.examples.config.js
+++ rollup.examples.config.js
@@ -1,6 +1,8 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import styles from 'rollup-plugin-styles';
-import { globals } from './rollup.src.config';
+
+export const globals = id =>
+  id.indexOf('@lumino/') === 0 ? id.replace('@lumino/', 'lumino_') : id;
 
 export function createRollupConfig(options) {
   return {
