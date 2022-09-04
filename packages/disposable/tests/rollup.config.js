01406c3b4d21db3a9b4de477ab2b5b77831795b9
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-09-04 16:38:56 +0200

    remove webpack from remaining packages

---
+++ packages/disposable/tests/rollup.config.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Jupyter Development Team.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
+import { createRollupConfig } from '../../../rollup.tests.config';
+const rollupConfig = createRollupConfig();
+export default rollupConfig;
