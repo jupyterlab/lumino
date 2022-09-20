7adcc2f084bb4470edda9a4086dedad7796c032b
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-09-20 09:19:29 +0200

    Add plugin description

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -43,6 +43,15 @@ export interface IPlugin<T extends Application, U> {
   id: string;
 
   /**
+   * Plugin description.
+   * 
+   * #### Notes
+   * This can be used to provide user documentation on the feature
+   * brought by a plugin.
+   */
+  description?: string;
+
+  /**
    * Whether the plugin should be activated on application start.
    *
    * #### Notes
@@ -171,6 +180,17 @@ export class Application<T extends Widget = Widget> {
    */
   get started(): Promise<void> {
     return this._delegate.promise;
+  }
+
+  /**
+   * Get a plugin description.
+   *
+   * @param id - The ID of the plugin of interest.
+   *
+   * @returns The plugin description.
+   */
+  getPluginDescription(id: string): string {
+    return this._plugins.get(id)?.description ?? '';
   }
 
   /**
@@ -682,6 +702,11 @@ namespace Private {
     readonly id: string;
 
     /**
+     * The description of the plugin.
+     */
+    readonly description: string;
+
+    /**
      * Whether the plugin should be activated on application start.
      */
     readonly autoStart: boolean;
@@ -733,8 +758,9 @@ namespace Private {
    * Create a normalized plugin data object for the given plugin.
    */
   export function createPluginData(plugin: IPlugin<any, any>): IPluginData {
-    return {
+    return Object.freeze({
       id: plugin.id,
+      description: plugin.description ?? '',
       service: null,
       promise: null,
       activated: false,
@@ -744,7 +770,7 @@ namespace Private {
       autoStart: plugin.autoStart ?? false,
       requires: plugin.requires ? plugin.requires.slice() : [],
       optional: plugin.optional ? plugin.optional.slice() : []
-    };
+    });
   }
 
   /**
