d80cf77e2c49f810c7539611c8c116c57579ab24
Steven Silvester <steven.silvester@ieee.org>
2022-09-23 09:58:37 -0500

    switch to pydata sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,8 @@ extensions = [
     'myst_parser',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'sphinx_copybutton'
+    'sphinx_copybutton',
+
 ]
 
 myst_enable_extensions = ["html_image"]
@@ -156,9 +157,7 @@ def build_examples(out_dir):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme ="pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
