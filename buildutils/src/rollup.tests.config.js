e4f5ac1bf9ae1880068b8c321bf599b9f51d4676
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-01-04 11:35:51 +0100

    Upgrade dev tools (#512)
    * Upgrade dev libraries
    
    * Upgrade to rollup v3
    
    * Use playwright instead of puppeteer
    
    * Upgrade lerna
    
    * Upgrade lint-staged
    
    * Ignore buildutils for documentation
    
    * Fix license header check
    
    * Automatic application of license header
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ buildutils/src/rollup.tests.config.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Jupyter Development Team.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export function createRollupTestConfig(options) {
+  return {
+    input: './lib/index.spec.js',
+    output: {
+      file: './lib/bundle.test.js'
+    },
+    plugins: [commonjs(), nodeResolve()]
+  };
+}
