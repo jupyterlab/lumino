2360bb4c77d49d0688574088c851ee86de633e8a
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-02-14 11:25:45 +0100

    Add minimal tests on accessibility

--- packages/widgets/tests/src/accordionpanel.spec.ts
+++ packages/widgets/tests/src/accordionpanel.spec.ts
@@ -77,6 +77,37 @@ describe('@lumino/widgets', () => {
       });
     });
 
+    describe('#accessibility()', () => {
+      it('should create a widget ID if it does not exist', () => {
+        let panel = new LogAccordionPanel();
+        let w1 = new Widget();
+        let w2 = new Widget();
+
+        // Expects a widget ID to be created.
+        expect(!w1.id);
+        panel.addWidget(w1);
+        expect(w1.id);
+
+        // Expects the widget ID to be unchanged.
+        w2.id = 'test_id';
+        panel.addWidget(w2);
+        expect(w2.id === 'test_id');
+      });
+
+      it('should link the widget to its title', () => {
+        let layout = new AccordionLayout({ renderer });
+        let panel = new LogAccordionPanel({ layout });
+        let w = new Widget();
+        panel.addWidget(w);
+
+        expect(layout.titles[0].getAttribute('aria-controls') === w.id);
+        expect(
+          layout.widgets[0].node.getAttribute('aria-labelledby') ===
+            layout.titles[0].id
+        );
+      });
+    });
+
     describe('#dispose()', () => {
       it('should dispose of the resources held by the panel', () => {
         let panel = new LogAccordionPanel();
