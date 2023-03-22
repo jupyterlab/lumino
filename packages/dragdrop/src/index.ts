18ac2fbe914b0846a9f398e94c79823e2948d6c7
Afshin Taylor Darian <git@darian.email>
2023-03-22 14:47:17 +0000

    Remove `IDragEvent` deprecated tag (#561)
    * Remove IDrageEvent deprecated tag
    
    * Keep the comment without the deprecated tag

--- packages/dragdrop/src/index.ts
+++ packages/dragdrop/src/index.ts
@@ -38,8 +38,8 @@ export type SupportedActions =
  * events, a drop target must cancel the `'lm-dragenter'` event by
  * calling the event's `preventDefault()` method.
  *
- * @deprecated This interface will be deprecated in @lumino/dragdrop@^2.0.0 in favor
- * of ``Drag.Event``.
+ * This interface will be deprecated in @lumino/dragdrop@^2.0.0 in favor of
+ * `Drag.Event`.
  */
 export interface IDragEvent extends MouseEvent {
   /**
