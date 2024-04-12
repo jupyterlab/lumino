c00588537142ca375e7553bea64063d99ccb8e82
Frédéric Collonval <fcollonval@users.noreply.github.com>
2024-04-12 09:50:09 +0200

    Align typedoc config with JupyterLab

--- typedoc.js
+++ typedoc.js
@@ -5,30 +5,25 @@
 
 module.exports = {
   entryPoints: [
-    'packages/algorithm/src/index.ts',
-    'packages/application/src/index.ts',
-    'packages/collections/src/index.ts',
-    'packages/commands/src/index.ts',
-    'packages/coreutils/src/index.ts',
-    'packages/datagrid/src/index.ts',
-    'packages/disposable/src/index.ts',
-    'packages/domutils/src/index.ts',
-    'packages/dragdrop/src/index.ts',
-    'packages/keyboard/src/index.ts',
-    'packages/messaging/src/index.ts',
-    'packages/polling/src/index.ts',
-    'packages/properties/src/index.ts',
-    'packages/signaling/src/index.ts',
-    'packages/virtualdom/src/index.ts',
-    'packages/widgets/src/index.ts'
+    'packages/algorithm',
+    'packages/application',
+    'packages/collections',
+    'packages/commands',
+    'packages/coreutils',
+    'packages/datagrid',
+    'packages/disposable',
+    'packages/domutils',
+    'packages/dragdrop',
+    'packages/keyboard',
+    'packages/messaging',
+    'packages/polling',
+    'packages/properties',
+    'packages/signaling',
+    'packages/virtualdom',
+    'packages/widgets'
   ],
-  exclude: [
-    'buildutils',
-    'examples/**',
-    '**/tests/**',
-    'packages/default-theme'
-  ],
-  includeVersion: true,
+  entryPointStrategy: 'packages',
+  includeVersion: false,
   name: '@lumino',
   out: 'docs/source/api',
   readme: 'README.md',
@@ -37,6 +32,5 @@ module.exports = {
     Jupyter: 'https://jupyter.org'
   },
   titleLink: 'https://lumino.readthedocs.io/en/latest/',
-  plugin: ['typedoc-plugin-mdn-links'],
-  tsconfig: 'tsconfig.docs.json'
+  plugin: ['typedoc-plugin-mdn-links']
 };
