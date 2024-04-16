87f4705323ba118d51d18ec203e5b08ee8a38926
martinRenou <martin.renou@gmail.com>
2024-04-16 09:03:33 +0200

    Add model

--- packages/datagrid/tests/src/datagrid.spec.ts
+++ packages/datagrid/tests/src/datagrid.spec.ts
@@ -2,13 +2,41 @@
 // Distributed under the terms of the Modified BSD License.
 import { expect } from 'chai';
 
-import { DataGrid } from '@lumino/datagrid';
+import { DataGrid, DataModel } from '@lumino/datagrid';
+
+class LargeDataModel extends DataModel {
+  rowCount(region: DataModel.RowRegion): number {
+    return region === 'body' ? 1_000_000_000_000 : 2;
+  }
+
+  columnCount(region: DataModel.ColumnRegion): number {
+    return region === 'body' ? 1_000_000_000_000 : 3;
+  }
+
+  data(region: DataModel.CellRegion, row: number, column: number): any {
+    if (region === 'row-header') {
+      return `R: ${row}, ${column}`;
+    }
+    if (region === 'column-header') {
+      return `C: ${row}, ${column}`;
+    }
+    if (region === 'corner-header') {
+      return `N: ${row}, ${column}`;
+    }
+    return `(${row}, ${column})`;
+  }
+}
 
 describe('@lumino/datagrid', () => {
   let datagrid: DataGrid;
+  let model: LargeDataModel;
+
   beforeEach(() => {
     datagrid = new DataGrid();
+    model = new LargeDataModel();
+    datagrid.dataModel = model;
   });
+
   describe('DataGrid', () => {
     describe('currentViewport()', () => {
       it('should return the viewport', () => {
