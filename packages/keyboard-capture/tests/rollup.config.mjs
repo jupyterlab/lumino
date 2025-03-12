989c318fb50d419f27aed656a0e5a59acbccafde
Vidar Tonaas Fauske <510760+vidartf@users.noreply.github.com>
2025-03-12 16:55:45 +0000

    Move all capture code to example
    This avoids a new package, and the code is small enough that
    it fits well as an example.

--- packages/keyboard-capture/tests/rollup.config.mjs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Jupyter Development Team.
- * Distributed under the terms of the Modified BSD License.
- */
-
-import { createRollupTestConfig } from '@lumino/buildutils';
-const rollupConfig = createRollupTestConfig();
-export default rollupConfig;
