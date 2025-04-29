928f9d762fff744ce0c961ecadab2d76c9dad794
Nisha Nair <102628718+nkn2022@users.noreply.github.com>
2025-04-29 03:21:18 -0700

    fix: improve accessibility for menu items with toggle state (#753)
    * fix: improve accessibility for menu items with toggle state
    
    * fix: clean up
    
    ---------
    
    Co-authored-by: Nisha Nair <nknair@Nishas-MacBook-Pro.local>

--- packages/widgets/src/menu.ts
+++ packages/widgets/src/menu.ts
@@ -1369,7 +1369,12 @@ export namespace Menu {
           if (!data.item.isEnabled) {
             aria['aria-disabled'] = 'true';
           }
-          aria.role = 'menuitem';
+          if (data.item.isToggled) {
+            aria.role = 'menuitemcheckbox';
+            aria['aria-checked'] = 'true';
+          } else {
+            aria.role = 'menuitem';
+          }
       }
       return aria;
     }
