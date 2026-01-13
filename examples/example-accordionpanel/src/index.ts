8517e9798da5bb3ca8de77a7c21b521ba2d65a88
Kaushik Harsha <kaushikharsha2020@gmail.com>
2026-01-14 00:27:08 +0530

    Update index.ts

--- examples/example-accordionpanel/src/index.ts
+++ examples/example-accordionpanel/src/index.ts
@@ -1,10 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
-
 import { Message } from '@lumino/messaging';
-
 import { AccordionPanel, BoxPanel, Widget } from '@lumino/widgets';
-
 import '../style/index.css';
 
 class ContentWidget extends Widget {
@@ -40,48 +37,52 @@ class ContentWidget extends Widget {
 }
 
 function main(): void {
-  // Default accordion (existing behavior)
-  const defaultAccordion = new AccordionPanel();
-  defaultAccordion.id = 'accordion-default';
+  // Default accordion with 'last-open' behavior (original)
+  const accordionDefault = new AccordionPanel();
+  accordionDefault.id = 'accordion-default';
 
   const r1 = new ContentWidget('Red');
   const b1 = new ContentWidget('Blue');
   const g1 = new ContentWidget('Green');
 
-  defaultAccordion.addWidget(r1);
-  defaultAccordion.addWidget(b1);
-  defaultAccordion.addWidget(g1);
+  accordionDefault.addWidget(r1);
+  accordionDefault.addWidget(b1);
+  accordionDefault.addWidget(g1);
 
-  BoxPanel.setStretch(defaultAccordion, 1);
+  BoxPanel.setStretch(accordionDefault, 1);
 
-  // In-place collapse accordion (NEW behavior)
-  const inPlacePanel = new AccordionPanel({
-    collapseMode: 'in-place'
+  // New accordion with 'in-place' collapse mode
+  const accordionInPlace = new AccordionPanel({
+    collapseMode: 'in-place'  // NEW: Sections collapse in-place without redistributing space
   });
-  inPlacePanel.id = 'accordion-in-place';
+  accordionInPlace.id = 'accordion-inplace';
 
   const y1 = new ContentWidget('Yellow');
   const p1 = new ContentWidget('Purple');
   const o1 = new ContentWidget('Orange');
 
-  inPlacePanel.addWidget(y1);
-  inPlacePanel.addWidget(p1);
-  inPlacePanel.addWidget(o1);
+  accordionInPlace.addWidget(y1);
+  accordionInPlace.addWidget(p1);
+  accordionInPlace.addWidget(o1);
 
-  BoxPanel.setStretch(inPlacePanel, 1);
+  BoxPanel.setStretch(accordionInPlace, 1);
 
-  // Main container
-  const main = new BoxPanel({ direction: 'left-to-right', spacing: 0 });
+  // Create main layout with both accordions side by side
+  const main = new BoxPanel({ direction: 'left-to-right', spacing: 10 });
   main.id = 'main';
-
-  main.addWidget(defaultAccordion);
-  main.addWidget(inPlacePanel);
+  main.addWidget(accordionDefault);
+  main.addWidget(accordionInPlace);
 
   window.onresize = () => {
     main.update();
   };
-  
+
   Widget.attach(main, document.body);
+
+  // Demo info
+  console.log('Two Accordion Panels loaded!');
+  console.log('Left: Default "last-open" mode - last section expands when others collapse');
+  console.log('Right: New "in-place" mode - sections collapse independently without affecting others');
 }
 
 window.onload = main;
